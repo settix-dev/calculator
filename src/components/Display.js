dcea44f64407583adf2a448164a58b5b5194bc48
settix-dev <settimbalamech@gmail.com>
2021-11-07 10:07:03 +0300

    Update Display.js

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,20 +1,16 @@
-import PropTypes from 'prop-types'
-import React from 'react'
+import PropTypes from "prop-types";
+import React from "react";
 
-const Display = ({result}) => {
-    return (
-        <div>
-           {result} 
-        </div>
-    )
-}
+const Display = ({ result }) => {
+  return <div className="display-result">{result}</div>;
+};
 
 Display.defaultProps = {
-    result: '0'
-}
+  result: "0",
+};
 
 Display.propTypes = {
-    result: PropTypes.string
-}
+  result: PropTypes.string,
+};
 
-export default Display
+export default Display;
